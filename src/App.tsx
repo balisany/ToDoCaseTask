1c636dfa40a5a0eaea6fcb7cf0020fe2fc6065f9
Delo Balisany <balisany@gmail.com>
2024-02-23 13:24:20 +0100

    add simple filter func

--- src/App.tsx
+++ src/App.tsx
@@ -1,4 +1,4 @@
-import { useState } from "react";
+import { useEffect, useState } from "react";
 import TodoList from "./components/TodoList";
 import TodoForm from "./components/TodoForm";
 import ToDoMockData from "./data";
@@ -7,6 +7,11 @@ import TodoItem from "./types";
 function App() {
   const initialMockData = ToDoMockData;
   const [todos, setTodos] = useState<TodoItem[]>(initialMockData);
+  const [filteredToDos, setFilteredToDos] = useState<TodoItem[]>(todos);
+
+  useEffect(() => {
+    setFilteredToDos(todos);
+  }, [todos]);
 
   const onSubmit = (title: string) => {
     setTodos((currentTodos) => {
@@ -41,12 +46,69 @@ function App() {
 
   const onEdit = (id: string) => {};
 
+  const countFilteredTasks = (type: "ALL" | "ACTIVE" | "COMPLETED"): number => {
+    const filteredByType =
+      type === "ALL"
+        ? todos
+        : todos.filter((todo) =>
+            type === "COMPLETED" ? todo.completed : !todo.completed
+          );
+
+    return filteredByType.length;
+  };
+
+  const onFilter = (type: "ALL" | "ACTIVE" | "COMPLETED") => {
+    const filtered =
+      type === "ALL"
+        ? todos
+        : todos.filter((todo) =>
+            type === "COMPLETED" ? todo.completed : !todo.completed
+          );
+
+    setFilteredToDos(filtered);
+  };
+
+  const CounterLabel = (props: { count: number }) => {
+    return (
+      <span className="inline-flex items-center justify-center w-6 h-6 ms-1 text-xs font-semibold text-blue-800 bg-blue-200 rounded-full">
+        {Number(props.count)}
+      </span>
+    );
+  };
+
   return (
     <div className="container">
+      <div className="p-1 m-4  flex text-center content-center items-center gap-6">
+        <button
+          type="button"
+          className="hover:underline"
+          onClick={() => onFilter("ALL")}
+        >
+          All
+          <CounterLabel count={countFilteredTasks("ALL")} />
+        </button>
+        <button
+          type="button"
+          className="hover:underline"
+          onClick={() => onFilter("ACTIVE")}
+        >
+          Active
+          <CounterLabel count={countFilteredTasks("ACTIVE")} />
+        </button>
+        <button
+          type="button"
+          className="hover:underline"
+          onClick={() => onFilter("COMPLETED")}
+        >
+          Completed
+          <CounterLabel count={countFilteredTasks("COMPLETED")} />
+        </button>
+      </div>
+
       <div className="bg-white rounded shadow p-6 m-4 sm:w-full md:w-3/4 lg:w-3/4 lg:max-w-lg">
         <TodoForm onSubmit={onSubmit} />
         <TodoList
-          data={todos}
+          data={filteredToDos}
           onEdit={onEdit}
           onDelete={onDelete}
           onToggle={onToggle}
